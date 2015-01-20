c42a0ff11fdf7a985a4177b5fb38000bee146e35
Marc Leeman <marc.leeman@gmail.com>
2015-01-20 06:56:50 +0100

    restore original c/h location files

---
+++ src/gstbarcomgs_common.c
@@ -0,0 +1,139 @@
+/* ex: set tabstop=2 shiftwidth=2 expandtab: */
+/** \class gstsbarcomgs_common
+ *
+ * \brief common functions to use in the gst barco bins
+ *
+ * \author Marc Leeman
+ * \date 2009
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gst/gst.h>
+#include <string.h>
+
+#ifdef WIN32
+#include "../src/gstbarcomgs_common.h"
+#else
+#include "src/gstbarcomgs_common.h"
+#endif
+/** 
+ * @brief return TRUE/FALSE based on string values accepted in GST
+ * 
+ * @param value: string to analyse
+ * 
+ * @return TRUE if GST true value; false otherwise
+ */
+gboolean
+gst_barco_query_to_boolean (gchar * value)
+{
+  gchar *down;
+
+  g_return_val_if_fail (value != NULL, FALSE);
+
+  down = g_ascii_strdown (value, -1);
+  if (g_strcmp0 (down, "true") == 0 ||
+      g_strcmp0 (down, "1") == 0 || g_strcmp0 (down, "on") == 0) {
+    g_free (down);
+    return TRUE;
+  }
+  g_free (down);
+  return FALSE;
+}
+
+gboolean
+gst_barco_is_ipv4 (SoupURI * uri)
+{
+  gboolean res = TRUE;
+
+  if (uri && 0 != *(uri->host)) {
+    GInetAddress *addr = g_inet_address_new_from_string (uri->host);
+    if (g_inet_address_get_family (addr) == G_SOCKET_FAMILY_IPV4) {
+      g_print ("IPv4 based on %s\n", uri->host);
+    } else {
+      g_print ("No IPv4 based on %s\n", uri->host);
+      res = FALSE;
+    }
+    g_object_unref (addr);
+  }
+
+  return res;
+}
+
+void
+gst_barco_parse_uri (GObject * obj, SoupURI * uri, GstDebugCategory * cat)
+{
+  if (uri && uri->query) {
+    GHashTable *hash_table = NULL;
+    GList *keys = NULL, *key;
+
+    hash_table = soup_form_decode (uri->query);
+    keys = g_hash_table_get_keys (hash_table);
+
+    for (key = keys; key; key = key->next) {
+      GParamSpec *spec;
+      spec = g_object_class_find_property (G_OBJECT_GET_CLASS (obj), key->data);
+      if (spec) {
+        switch (spec->value_type) {
+          case G_TYPE_BOOLEAN:
+            g_object_set (obj, key->data, gst_barco_query_to_boolean ((gchar *)
+                    g_hash_table_lookup (hash_table, key->data)), NULL);
+            break;
+          case G_TYPE_INT:
+            g_object_set (obj, key->data,
+                (gint) g_ascii_strtoll (
+                    (gchar *) g_hash_table_lookup (hash_table, key->data),
+                    NULL, 0), NULL);
+            break;
+          case G_TYPE_UINT:
+            g_object_set (obj, key->data,
+                (guint) g_ascii_strtoull (
+                    (gchar *) g_hash_table_lookup (hash_table, key->data),
+                    NULL, 0), NULL);
+            break;
+          case G_TYPE_INT64:
+            g_object_set (obj, key->data,
+                g_ascii_strtoll (
+                    (gchar *) g_hash_table_lookup (hash_table, key->data),
+                    NULL, 0), NULL);
+            break;
+          case G_TYPE_UINT64:
+            g_object_set (obj, key->data,
+                g_ascii_strtoull (
+                    (gchar *) g_hash_table_lookup (hash_table, key->data),
+                    NULL, 0), NULL);
+            break;
+          case G_TYPE_STRING:
+            g_object_set (obj, key->data,
+                (gchar *) g_hash_table_lookup (hash_table, key->data), NULL);
+            break;
+          default:
+            /* Not fundamental types or unknown */
+            if (spec->value_type == GST_TYPE_CAPS) {
+              GstCaps *caps = gst_caps_from_string (
+                  (gchar *) g_hash_table_lookup (hash_table, key->data));
+              g_object_set (obj, key->data, caps, NULL);
+              gst_caps_unref (caps);
+            } else {
+              GST_CAT_WARNING_OBJECT (cat, obj,
+                  "Unknown type or not yet supported: %s "
+                  "(Maybe it should be added)", g_type_name (spec->value_type));
+              continue;
+            }
+            break;
+        }
+        GST_CAT_LOG_OBJECT (cat, obj, "Set property %s: %s",
+            (const gchar *) key->data,
+            (const gchar *) g_hash_table_lookup (hash_table, key->data));
+      } else
+        GST_CAT_LOG_OBJECT (cat, obj, "Property %s not supported",
+            (const gchar *) key->data);
+    }
+
+    g_list_free (keys);
+    g_hash_table_destroy (hash_table);
+  }
+}
---
+++ src/gstrtpsrc.h
@@ -0,0 +1,61 @@
+#ifndef _GST_RTP_SRC_H_
+#define _GST_RTP_SRC_H_
+
+#include "gst/gst.h"
+#include <libsoup/soup.h>
+
+G_BEGIN_DECLS
+#define GST_TYPE_RTP_SRC             (gst_rtp_src_get_type ())
+#define GST_RTP_SRC(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), GST_TYPE_RTP_SRC, GstRtpSrc))
+#define GST_RTP_SRC_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), GST_TYPE_RTP_SRC, GstRtpSrcClass))
+#define GST_IS_RTP_SRC(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GST_TYPE_RTP_SRC))
+#define GST_IS_RTP_SRC_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), GST_TYPE_RTP_SRC))
+#define GST_RTP_SRC_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), GST_TYPE_RTP_SRC, GstRtpSrcClass))
+typedef struct _GstRtpSrcClass GstRtpSrcClass;
+typedef struct _GstRtpSrc GstRtpSrc;
+
+struct _GstRtpSrcClass
+{
+  GstBinClass parent_class;
+};
+
+struct _GstRtpSrc
+{
+  GstBin parent_instance;
+
+  SoupURI *uri, *rtcp_remote_uri;
+  gchar *last_uri;
+  gchar *encoding_name;
+  guint pt_select;
+  guint pt_change;
+  guint ssrc_select;
+  guint ssrc_change;
+  gchar *multicast_iface;
+  guint buffer_size;
+  guint latency;
+  gboolean encrypt;
+  guint32 key_derivation_rate;
+  guint64 timeout;
+
+  gboolean enable_rtcp;
+
+  GstElement *rtp_src;
+  GstElement *rtcp_src;
+  GstElement *rtcp_sink;
+  GstElement *rtpbin;
+  GstElement *rtpheaderchange;
+  GstElement *rtpdecrypt;
+
+  gint n_ptdemux_pads;
+  gint n_rtpbin_pads;
+  GstPad *ghostpad;
+};
+
+GType
+gst_rtp_src_get_type (void)
+    G_GNUC_CONST;
+
+     gboolean rtp_src_init (GstPlugin * plugin);
+
+G_END_DECLS
+#endif /* _GST_RTP_SRC_H_ */
