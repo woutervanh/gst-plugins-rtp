e06d651b06d64cc1aa6399c1ab8339a8a17b4ec0
Marc Leeman <marc.leeman@gmail.com>
2018-06-08 16:54:30 +0200

    meson: copy paste naming corrected

--- env.sh
+++ env.sh
@@ -9,7 +9,6 @@ export SKIP_BUILD_DEPS=TRUE
 export LIST_OF_VARS_TO_REPLACE='${BLACKLISTED_PACKAGES},${LOCAL_CD},${CD_VERSION},${PACKAGES_TO_INSTALL},${SYSTEM_NAME},${BASEOS_DISTRO_REPO},${DEBIAN_DISTRO}'
 export ISO_CFG=${SYSTEM_NAME,,}-${BASEOS_DISTRO_REPO}
 export PACKAGING_SWITCHES="-g -s -l"
-export DO_APT_UPGRADE=
 ################# Test Setup ####################
 export TEST_DIR=jenkins-test
 export TEST_COMPOSE_LIST="-f ./docker-compose.yml"
