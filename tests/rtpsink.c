d9a532b4aaf76f0aaebb6e4d6d208215efd3a2ca
Marc Leeman <marc.leeman@gmail.com>
2018-07-20 12:04:30 +0200

    tests: test uri rtpsink/rtpsrc

--- tests/rtpsink.c
+++ tests/rtpsink.c
@@ -1,5 +1,59 @@
 #include <gst/check/gstcheck.h>
 
+/* Performs a basic check whether we are able to map URI query parameters to
+ * properties in the rtpsink. Note that it doesn't test whether these are also
+ * propagated to the correct child elements or anything. */
+GST_START_TEST (test_uri_to_properties)
+{
+  GstElement *rtpsink;
+
+  gchar *bind_address;
+  guint cidr;
+  gint bind_port, ttl, ttl_mc, src_port;
+
+  rtpsink = gst_element_factory_make ("rtpsink", NULL);
+
+  /* Sets properties to non-default values (make sure this stays in sync) */
+  g_object_set (rtpsink, "uri", "rtp://1.230.1.2?"
+      "cidr=24"
+      "&bind-address=1.2.3.4"
+      "&bind-port=5678"
+      "&ttl=8"
+      "&ttl-mc=9",
+      NULL);
+
+  g_object_get (rtpsink,
+      "cidr", &cidr,
+      "bind_port", &bind_port,
+      "ttl", &ttl,
+      "ttl_mc", &ttl_mc,
+      "bind-address", &bind_address,
+      "src-port", &src_port,
+      NULL);
+
+  /* Make sure these values are in sync with the one from the URI. */
+  g_assert_cmpint (ttl , ==, 8);
+  g_assert_cmpint (ttl_mc , ==, 9);
+  g_assert_cmpuint (cidr , ==, 24);
+  g_assert_cmpuint (bind_port, ==, 5678);
+  g_assert_cmpuint (src_port, ==, 5678);
+  g_assert_cmpstr (bind_address, ==, "1.2.3.4");
+
+  g_object_set (rtpsink, "src-port", 1234,
+      NULL);
+
+  g_object_get (rtpsink,
+      "src-port", &src_port,
+      "bind-port", &bind_port,
+      NULL);
+
+  g_assert_cmpuint (src_port, ==, 1234);
+  g_assert_cmpuint (bind_port, ==, 1234);
+
+  gst_object_unref (rtpsink);
+}
+GST_END_TEST;
+
 GST_START_TEST (test_pads)
 {
   GstElement *element;
@@ -62,6 +116,8 @@ rtpsink_suite (void)
   TCase *tc_chain = tcase_create ("general");
 
   suite_add_tcase (s, tc_chain);
+  tcase_add_test (tc_chain, test_uri_to_properties);
+
   /*tcase_add_test (tc_chain, test_pads);*/
   /*tcase_add_test (tc_chain, test_pads_localhost);*/
   /*tcase_add_test (tc_chain, test_pads_localhost_3_slashes);*/
--- tests/rtpsrc.c
+++ tests/rtpsrc.c
@@ -1,5 +1,60 @@
 #include <gst/check/gstcheck.h>
 
+/* Performs a basic check whether we are able to map URI query parameters to
+ * properties in the rtpsrc. Note that it doesn't test whether these are also
+ * propagated to the correct child elements or anything. */
+GST_START_TEST (test_uri_to_properties)
+{
+  GstElement *rtpsrc;
+  guint buffer_size, latency, pt_change, pt_select, ssrc_change, ssrc_select, ttl_mc;
+  guint64 timeout;
+  gchar *caps, *encoding_name, multicast_iface;
+  gboolean enable_rtcp;
+
+
+  rtpsrc = gst_element_factory_make ("rtpsrc", NULL);
+
+  /* Sets properties to non-default values (make sure this stays in sync) */
+  g_object_set (rtpsrc, "uri", "rtp://1.230.1.2?"
+      "buffer-size=8192"
+      "&caps=application/x-rtp"
+      "&latency=300"
+      "&multicast-iface=eth0"
+      "&pt-change=96"
+      "&pt-select=98"
+      "&ssrc-change=123456"
+      "&ssrc-select=654321"
+      "&timeout=10000"
+      "&ttl-mc=9",
+      NULL);
+
+  g_object_get (rtpsrc,
+      "buffer-size", &buffer_size,
+      "caps", &caps,
+      "latency", &latency,
+      "multicast-iface", &multicast_iface,
+      "pt-change", &pt_change,
+      "pt-select", &pt_select,
+      "ssrc-change", &ssrc_change,
+      "ssrc-select", &ssrc_select,
+      "timeout", &timeout,
+      "ttl-mc", &ttl_mc,
+      NULL);
+
+  /* Make sure these values are in sync with the one from the URI. */
+  g_assert_cmpuint (buffer_size, ==, 8192);
+  g_assert_cmpuint (latency, ==, 300);
+  g_assert_cmpuint (pt_change, ==, 96);
+  g_assert_cmpuint (pt_select, ==, 98);
+  g_assert_cmpuint (ssrc_change, ==, 123456);
+  g_assert_cmpuint (ssrc_select, ==, 654321);
+  g_assert_cmpuint (timeout , ==, 10000);
+  g_assert_cmpint (ttl_mc , ==, 9);
+
+  gst_object_unref (rtpsrc);
+}
+GST_END_TEST;
+
 GST_START_TEST (test_pads)
 {
   GstElement *element;
@@ -20,6 +75,7 @@ rtpsrc_suite (void)
 
   suite_add_tcase (s, tc_chain);
   tcase_add_test (tc_chain, test_pads);
+  tcase_add_test (tc_chain, test_uri_to_properties);
 
   return s;
 }
